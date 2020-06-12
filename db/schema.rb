e3233c83fd0a8c0ecfca57a2581792689b623625
beka <mr.begzodjon96@mail.ru>
2020-06-12 19:01:32 +0300

    Checkbox added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_153535) do
+ActiveRecord::Schema.define(version: 2020_06_11_225106) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_06_11_153535) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "events", "users", column: "creator_id"
