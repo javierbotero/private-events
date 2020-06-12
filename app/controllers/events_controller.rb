b080a945d3463709b77229f075e9326ad9076533
beka <mr.begzodjon96@mail.ru>
2020-06-12 23:31:11 +0300

    Invite to ivent created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,9 @@
 class EventsController < ApplicationController
   before_action :required_user, only: [:new]
 
-  def new; end
+  def new
+    @users = User.where.not(id: current_user.id)
+  end
 
   def index
     @events = Event.all.order(created_at: :DESC)
@@ -9,9 +11,13 @@ class EventsController < ApplicationController
 
   def create
     event = current_user.events.build(event_params)
-
-    if event.save
+    users = User.where.not(id:current_user.id)
+    if event.save && attendanse_params != ['0'] 
       flash[:success] = 'Event created successfully!'
+      attendanse_params.each do |user|
+        a = event.user_attendees.build(attendee_id: user.to_i)
+        a.save
+      end
       redirect_to event_path(event)
     else
       flash[:danger] = "Event didn't created!"
@@ -21,11 +27,20 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find_by(id: params[:id])
+    @users = User.all
+  end
+
+  def update
+    debugger
   end
 
   protected
 
   def event_params
     params.require(:event).permit(:title, :date, :description)
+  end
+
+  def attendanse_params
+    params.require(:attendees)
   end
 end
